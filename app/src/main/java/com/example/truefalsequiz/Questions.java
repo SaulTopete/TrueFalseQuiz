7405dbfb51ef18e5e3003db23046741aba0ca115
Saul <saulftopete2@gmail.com>
2018-10-08 09:37:49 -0700

    anotha one

--- app/src/main/java/com/example/truefalsequiz/Questions.java
+++ app/src/main/java/com/example/truefalsequiz/Questions.java
@@ -1,4 +1,30 @@
 package com.example.truefalsequiz;
 
 public class Questions {
+    private String question;
+    private boolean answer;
+
+    @Override
+    public String toString() {
+        return "Questions{" +
+                "question='" + question + '\'' +
+                ", answer=" + answer +
+                '}';
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
 }
